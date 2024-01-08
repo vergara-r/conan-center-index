0e6eb52a391fe6142a18f607a2ea20a9957e86a2
vergara-r <124825497+vergara-r@users.noreply.github.com>
2024-01-08 20:38:37 +0100

    Fix libffi compiation from windows with clang

--- recipes/libffi/all/conanfile.py
+++ recipes/libffi/all/conanfile.py
@@ -58,7 +58,6 @@ class LibffiConan(ConanFile):
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", default=False, check_type=str):
                 self.tool_requires("msys2/cci.latest")
-        if is_msvc(self) or (cross_building(self) and self._settings_build.os == "Windows"):
             self.tool_requires("automake/1.16.5")
 
     def source(self):
